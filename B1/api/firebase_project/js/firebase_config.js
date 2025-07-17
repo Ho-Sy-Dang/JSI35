a00b554bf17f15d26a7ce65c7ce96a2f29d6a25d
Ho Sy Dang <dangprograming@gmail.com>
2025-07-17 21:08:26 +0700

    b2

---
+++ B1/api/firebase_project/js/firebase_config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDAfHJ8pLYOQJ8xlYEUylRcWFm8lPJf7IQ",
+    authDomain: "jsi35-f46da.firebaseapp.com",
+    databaseURL: "https://jsi35-f46da-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "jsi35-f46da",
+    storageBucket: "jsi35-f46da.firebasestorage.app",
+    messagingSenderId: "900319334447",
+    appId: "1:900319334447:web:de5e582e68f13f3b1df6da",
+    measurementId: "G-GKGL0XJKCX"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
